fb7157df2a195812a0087f2a741f88e831fb8f9f
Priyam Gupta <prigupta8@publicisgroupe.net>
2018-12-29 19:13:47 +0530

    first-commit

---
+++ src/main/java/com/priyam/kafka/spingbootkafkaexample/SpingbootKafkaExampleApplication.java
@@ -0,0 +1,14 @@
+package com.priyam.kafka.spingbootkafkaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootKafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootKafkaExampleApplication.class, args);
+	}
+
+}
+
