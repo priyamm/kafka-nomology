535a0bd7a15ac87370e591c08c4bd1a04d866081
Priyam Gupta <prigupta8@publicisgroupe.net>
2018-12-29 20:26:52 +0530

    producer-added

---
+++ src/main/java/com/priyam/kafka/spingbootkafkaexample/model/User.java
@@ -0,0 +1,27 @@
+package com.priyam.kafka.spingbootkafkaexample.model;
+
+public class User {
+    private String name;
+    private String message;
+
+    public User(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
